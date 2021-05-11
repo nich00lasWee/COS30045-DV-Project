9beb7e73929cd7fa8257c904f56e7ae040b023f3
GabrielReynolds <103075616@student.swin.edu.au>
2021-05-11 18:08:26 +1000

    Improved responsiveness, cleaned code, implemented line graph and fixed issues reading data in PieChart
    TO-DO:
    - Resolve x-axis issue with line chart
    - Finish implementing responsiveness
    - Bubble chart

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,6 @@
+function pieChart(dataset, svg, cD) {    // waste production of each sector 2018-2019 (in tonnes)
 
-function pieChart(dataset, svg, h, cW) {
-
-  var sector = dataset.map(function(d) {return d.Sector})
-  var waste = dataset.map(function(d) {return d.WasteProduced})
-
-  sector.pop(); // remove null values
-  waste.pop();
-
-  svg.append("rect")
-    .attr("x", 1)
-    .attr("y", 1)
-    .attr("width", cW - 1)
-    .attr("height", h - 1); // prevents svg from clipping rectangle
-
-  svg.selectAll("rect")
-    .style("fill","white")
-    .style("stroke","black")
-    .style("stroke-width","1.5");
-
-  var outerRadius = cW / 2.3;  // reduces size of chart
+  var outerRadius = cD / 2.3;  // reduces size of chart
   var innerRadius = 0;
 
   var arc = d3.arc()
@@ -28,7 +10,9 @@ function pieChart(dataset, svg, h, cW) {
   var pie = d3.pie();
 
   var arcs = svg.selectAll("g.arc")
-    .data(pie(waste))
+    .data(pie(dataset.map(function(d) {
+      return d.WasteProduced;
+    })))
     .enter()
     .append("g")
     .attr("class","arc")
@@ -49,64 +33,113 @@ function pieChart(dataset, svg, h, cW) {
           .attr("x", 20)
           .attr("y", 20)
           .text(d.value)
-          for(j = 0; j < sector.length; j++) {
-            if(j == i)                            // FIX LATER: Should detect when position of data is equivalent to position of loop
-              console.log(sector[i]);             // Logs the relevant data. Will eventually display the relevant data (sector) via tooltip
+          for(j = 0; j < 10; j++) {
+            if(j == i)                                                 // FIX LATER: Should detect when position of data is equivalent to position of loop
+              console.log("placeholder - will fix later (pieChart)");             // Logs the relevant data. Will eventually display the relevant data (sector) via tooltip
           };
       })
       .on("mouseout", function(d) {
-        svg.selectAll("text").remove();
+        svg.selectAll("#tooltip").remove();
       })
     }
 
-function lineChart(svg, h, cW) {
+function lineChart(dataset, svg, sW, cD, x2, padding) {
 
-  svg.append("rect")
-    .attr("x", cW + 223.5)
-    .attr("y", 1)
-    .attr("width", cW - 1)
-    .attr("height", h - 1); // prevents svg from clipping rectangle
+  // Scales are likely incorrect currently - revisit upon issue
+  var xScale = d3.scaleOrdinal()
+    .domain([
+      d3.min(dataset, function(d) {return d.TimePeriod;}),
+      d3.max(dataset, function(d) {return d.TimePeriod;})
+    ])
+    .range([(x2 + padding), (x2 + cD) - padding]);
 
-  svg.selectAll("rect")
-    .style("fill","white")
-    .style("stroke","black")
-    .style("stroke-width","1.5");
+  var yScale = d3.scaleLinear()
+    .domain([540, d3.max(dataset, function(d) {console.log(parseInt(d.HouseholdWaste) + 200); return (parseInt(d.HouseholdWaste) + 20);})])
+    .range([cD - padding, padding / 2]);
+
+  var line = d3.line()
+    .x(function(d) {return xScale(d.TimePeriod);})
+    .y(function(d) {return yScale(d.HouseholdWaste) + padding / 2;});
+
+  svg.append("path")
+    .datum(dataset)
+    .attr("class","line")
+    .attr("d",line)
+    .style("fill","none")
+    .style("stroke","slategrey")
+    .style("stroke-width","1");
+
+  var xAxis = d3.axisBottom()
+    .ticks(4)
+    .scale(xScale);
+
+  var yAxis = d3.axisLeft()
+    .ticks(7)
+    .scale(yScale);
+
+  svg.append("g")
+      .attr("transform", "translate(0, " + (cD - padding) + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("transform", "translate(" + (x2 + padding) + ", 0)")
+      .call(yAxis);
 }
 
-function bubbleChart(svg, h, sW, cW) {
+function bubbleChart(svg, sW, cD) {
 
-  svg.append("rect")
-    .attr("x", (sW - cW - 1))
-    .attr("y", 1)
-    .attr("width", cW - 1)
-    .attr("height", h - 1); // prevents svg from clipping rectangle
 
-  svg.selectAll("rect")
-    .style("fill","white")
-    .style("stroke","black")
-    .style("stroke-width","1.5");
 }
 
 function init() {
 
-  var sW = 1411;  // width of <div>
-  var cW = 320;   // width of charts
-  var h = 320;
+  var sW = document.getElementById('other-vis').clientWidth;  // Width changes depending on monitor used - this ensures correct value is fetched
+  var cD = 0.25*sW;                                           // Each chart is allocated 25% of svg Width - remaining 25% is used for gaps
 
   var svg = d3.selectAll("#svg")
     .append("svg")
-    .attr("width", sW)
-    .attr("height", h );
+    .attr("viewBox", "0 0 " + sW + " " + cD ) // Viewbox for responsiveness
+
+  svg.append("rect")
+    .attr("x", 1)
+    .attr("y", 1)
+    .attr("width", cD - 1)
+    .attr("height", cD - 1); // prevents svg from clipping rectangle
+
+  svg.append("rect")
+    .attr("x", (sW - cD - 1))
+    .attr("y", 1)
+    .attr("width", cD - 1)
+    .attr("height", cD - 1); // prevents svg from clipping rectangle
+
+  var temp = (sW - (cD * 2)); // determines box location depending on svg width (as set by div)
+  var x2 = (temp + cD) / 2;
+  var padding = 40;
+
+  svg.append("rect")
+    .attr("x", x2)
+    .attr("y", 1)
+    .attr("width", cD - 1)
+    .attr("height", cD - 1); // prevents svg from clipping rectangle
+
+  svg.selectAll("rect")
+    .style("fill","white")
+    .style("stroke","black")
+    .style("stroke-width","1.5");
 
   d3.csv("data/pieChart.csv").then(function(data) {
     console.log(data);
     var dataset = data;
-    pieChart(dataset, svg, h, cW);
+    pieChart(dataset, svg, cD);
   })
 
-  lineChart(svg, h, cW);
+  d3.csv("data/lineChart.csv").then(function(data) {
+    console.log(data);
+    var dataset = data;
+    lineChart(dataset, svg, sW, cD, x2, padding);  // forgive the amount of parameters, I'll condense this later
+  })
 
-  bubbleChart(svg, h, sW, cW);
+  bubbleChart(svg, sW, cD);
 }
 
 window.onload = init;
