aa819e50518b9acfb5bdb0b683f87181d1a9b68b
GabrielReynolds <103075616@student.swin.edu.au>
2021-06-02 21:09:11 +1000

    Finished Pie Chart and Area Chart. Namely: - Added tooltips - Added figures - Fixed Color Scales

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,8 @@
-function pieChart(dataset, cD, svg) {    // waste production of each sector 2018-2019 (in tonnes)
+function pieChart(dataset, cD, svg, tooltip) {    // waste production of each sector 2018-2019 (in tonnes)
 
   var outerRadius = cD / 2.3;  // reduces size of chart
   var innerRadius = 0;
-
   var padding = cD / 15.07826086956522; // acheives equal spacing
-
-  var sectors = dataset.map(function(d) {return d.Sector;});
-
-
 
   var arc = d3.arc()
     .outerRadius(outerRadius)
@@ -24,39 +19,60 @@ function pieChart(dataset, cD, svg) {    // waste production of each sector 2018
     .attr("class","arc")
     .attr("transform","translate(" + (outerRadius + padding) + "," + (outerRadius + padding) + ")");  // position in center of rectangle
 
-  var color = d3.scaleOrdinal(d3.schemeCategory10);
+  // Likely to change later
+  var color = d3.scaleOrdinal()
+      .range(["#487f82",
+          "#4bada1",
+          "#4cb48f",
+          "#61b876",
+          "#82bb57",
+          "#a8b935",
+          "#d2b30f",
+          "#ffa600"]);
 
-  arcs.append("path")
+  var path = arcs.append("path")
       .attr("fill",function(d, i) {
           return color(i);
       })
       .attr("d", function(d, i) {
           return arc(d, i);
-      })
-      .on("mouseover", function(event, d, i) {
-        svg.append("text")
-          .attr("id","tooltip")
-          .attr("x", 20)
-          .attr("y", 20)
-          .text(d.value + " tonnes")
-        })
-      .on("mouseout", function(d) {
-        svg.selectAll("#tooltip").remove();
       });
 
-  svg.append("text")
-    .attr("x", 5)
-    .attr("y", (cD + 30))
-    .style("font-size", "0.75vw")
-    .style("font-style","italic")
-    .text("Waste production by industry, 2018 - 2019 (tonnes)");
+  path.on("mouseover", function(event, d, i) {
+    var darkColor = d3.rgb(d3.select(this).attr("fill")).darker(0.5);
+    d3.select(this).attr("fill",darkColor);
+  })
+  .on("mousemove", function(event, d) {
+    var sector;
+    var waste;
+
+    for(j = 0; j < dataset.length; j++)
+      if(dataset[j].WasteProduced == d.value)
+      {
+        sector = dataset[j].Sector;
+        waste = d.value + " tonnes";
+      }
+
+    var coordinates = d3.pointer(event);
+    return (tooltip.style("visibility","visible")
+              .html("<b>" + sector + "</b>" + "<br>" + waste.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+              .style("top", event.pageY + "px")
+              .style("left", (event.pageX + 20) + "px")
+              );
+  })
+  .on("mouseout", function(d) {
+    svg.selectAll("#tooltip").remove();
+    var lightColor = d3.rgb(d3.select(this).attr("fill")).brighter(0.5);
+    d3.select(this).attr("fill",lightColor)
+    return (tooltip.style("visibility","hidden"));
+  });
 }
 
-function lineChart(dataset, svg, sW, cD, x2, padding) { // Plastic production per year, 2016 - 2019 (millions of tonnes)
+function lineChart(dataset, svg, sW, cD, x2, padding, tooltip) { // Plastic production per year, 2016 - 2019 (millions of tonnes)
 
   var xData = dataset.map(function(d){return d.TimePeriod;});  // Maps X values to array for scale
 
-  var xScale = d3.scalePoint()  // Took a while to realise: had to feed an array into this scale for it to work correctly
+  var xScale = d3.scalePoint()
     .domain(xData)
     .range([(x2 + padding), (x2 + cD) - padding]);
 
@@ -77,9 +93,16 @@ function lineChart(dataset, svg, sW, cD, x2, padding) { // Plastic production pe
     .datum(dataset)
     .attr("class","area")
     .attr("d",area)
-    .style("fill","slategrey")
-    .style("stroke","slategrey")
+    .style("fill","#b5e48c")
     .style("stroke-width","1");
+
+  svg.append("path")
+    .datum(dataset)
+    .attr("class","line")
+    .attr("d",line)
+    .style("fill","none")
+    .style("stroke","#76c893")
+    .style("stroke-width","3");
 
   var xAxis = d3.axisBottom()
     .ticks(4)
@@ -97,12 +120,25 @@ function lineChart(dataset, svg, sW, cD, x2, padding) { // Plastic production pe
       .attr("transform", "translate(" + (x2 + padding) + ", 0)")
       .call(yAxis);
 
-  svg.append("text")
-    .attr("x", x2)
-    .attr("y", (cD + 30))
-    .style("font-size", "0.75vw")
-    .style("font-style","italic")
-    .text("Plastic waste production, 2016 - 2019 (per million tonnes)");
+  svg.selectAll("myCircles")
+    .data(dataset)
+    .enter()
+    .append("circle")
+      .attr("fill","#3A8C57")
+      .attr("cx", function(d) {return xScale(d.TimePeriod);})
+      .attr("cy", function(d) {return yScale(d.PlasticWaste);})
+      .attr("r",4.5)
+    .on("mouseover", function(event, d) {
+      var total = d.TotalWaste + " tonnes";
+      return (tooltip.style("visibility","visible")
+                .html(total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                .style("top", event.pageY + "px")
+                .style("left", (event.pageX + 20) + "px"))
+    })
+    .on("mouseout", function(d) {
+      svg.selectAll("#tooltip").remove();
+      return (tooltip.style("visibility","hidden"));
+    });
 }
 
 function bubbleChart(svg, sW, cD) {
@@ -111,52 +147,77 @@ function bubbleChart(svg, sW, cD) {
 
 function init() {
 
-  var sW = document.getElementById('sub-vis').clientWidth;  // Width changes depending on monitor used - this ensures correct value is fetched
-  var cD = 0.25 * sW;  // Each chart is allocated 25% of svg Width - remainder is used for gaps between
-
-  console.log(cD);
+  var pageWidth = document.getElementById("overview-vis").clientWidth;
+  var sW = (pageWidth / 100) * 57.38332058148432;
+  var cD = 0.25 * sW;                                             // Each chart is allocated 25% of svg Width - remainder is used for gaps between
 
   var svg = d3.selectAll("#sub-vis")
     .append("svg")
-    .attr("viewBox","0, 0 " + sW + " " + (cD + 50));
+    .attr("viewBox","0, 0, " + sW + ", " + (cD + 50));
 
-    svg.append("rect")
-      .attr("x", 1)
-      .attr("y", 1)
-      .attr("width", cD - 1)
-      .attr("height", cD - 1); // prevents svg from clipping rectangle
+  svg.append("rect")
+    .attr("x", 1)
+    .attr("y", 1)
+    .attr("width", cD)
+    .attr("height", cD - 1);  // prevents svg from clipping rectangle
 
-  var temp = (sW - cD); // determines box location depending on svg width (as set by div)
-  var x2 = temp / 2;   // calculates x coordinate by finding middle point of temp
+  var temp = (sW - cD);       // determines box location depending on svg width (as set by div)
+  var x2 = temp / 2;          // calculates x coordinate by finding middle point of temp
   var padding = 40;
 
   svg.append("rect")
     .attr("x", x2)
     .attr("y", 1)
     .attr("width", cD - 1)
-    .attr("height", cD - 1); // prevents svg from clipping rectangle
+    .attr("height", cD - 1);
 
   svg.append("rect")
     .attr("x", temp)
     .attr("y", 1)
     .attr("width", cD - 1)
-    .attr("height", cD - 1); // prevents svg from clipping rectangle
+    .attr("height", cD - 1);  // prevents svg from clipping rectangle
 
   svg.selectAll("rect")
     .style("fill","white")
     .style("stroke","black")
     .style("stroke-width","1.5");
 
+  // Prepares tooltip for later use
+  var tooltip = d3.select("#sub-vis")
+      .append("div")
+      .style("position", "absolute")
+      .style("visibility", "hidden")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "1px")
+      .style("border-radius", "5px")
+      .style("padding", "10px");
+
   d3.csv("dataset/pieChart.csv").then(function(data) {
-    console.log(data);
+
+    // Currently lacks responsiveness
+    d3.select('#pie-text')
+      .style("display","inline-block")
+      .style("width",cD + "px")
+      .style("position","relative")
+      .style("top","100px")
+      .style("left",((pageWidth - 1500) / 2) + "px");
+
     var dataset = data;
-    pieChart(dataset, cD, svg);
+    pieChart(dataset, cD, svg, tooltip);
   })
 
   d3.csv("dataset/lineChart.csv").then(function(data) {
-    console.log(data);
+
+    d3.select("#area-text")
+      .style("display","inline-block")
+      .style("width",cD + "px")
+      .style("position","relative")
+      .style("top","100px")
+      .style("left", (cD * 1.95) + "px");
+
     var dataset = data;
-    lineChart(dataset, svg, sW, cD, x2, padding);  // forgive the amount of parameters, I'll condense this later
+    lineChart(dataset, svg, sW, cD, x2, padding, tooltip);  // forgive the amount of parameters, I'll condense this later
  })
 
 //  bubbleChart(svg, sW, cD);
