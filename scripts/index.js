22b309ccf5626be04046114f272b3896a6f3315c
GabrielReynolds <103075616@student.swin.edu.au>
2021-06-13 15:31:02 +1000

    Comments and formatting

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,4 @@
 function pieChart(dataset, cD, svg, x1, tooltip) {
-<<<<<<< HEAD
-
-=======
->>>>>>> deb083edd2ce977eb05e8a472a0010927bf39359
   var outerRadius = cD / 2.3;  // reduces size of chart
   var innerRadius = 0;
   var padding = cD / 15.07826086956522; // acheives equal spacing
---
+++ scripts/shared.js
@@ -0,0 +1,30 @@
+var margin = {top: 25, right: 20, bottom: 75, left: 100};
+var width = 1500 - margin.left - margin.right;
+var height = 650 - margin.top - margin.bottom;
+
+// Method source: https://bl.ocks.org/mbostock/7555321
+// from: https://stackoverflow.com/questions/24784302/wrapping-text-in-d3
+
+function wrap(text, width) {
+    text.each(function() {
+      var text = d3.select(this),
+          words = text.text().split(/\s+/).reverse(),
+          word,
+          line = [],
+          lineNumber = 0,
+          lineHeight = 1.2, // ems
+          y = text.attr("y"),
+          dy = parseFloat(text.attr("dy")),
+          tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+      while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        if (tspan.node().getComputedTextLength() > width) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+        }
+      }
+    });
+} 
